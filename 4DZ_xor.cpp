80d0b9415156a1fcaf3bb02201a63f4ad1d2dfe3
Nadya V. Klimkovich <klimkovichna@gmail.com>
2015-11-01 17:52:12 +0300

    DZ 4

---
+++ 4DZ_xor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void XOR(int &x) 
+{
+	int tmp;
+	int a = 13, b = 15, c = 5;
+	tmp = x ^ (x << a);
+	tmp = tmp ^ (tmp >> b);
+	x = x ^ (tmp << c);
+}
+int main()
+{
+	system("pause");
+	return 0;
+}
